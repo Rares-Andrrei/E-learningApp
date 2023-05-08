ad2bc2cf74f3602169ae5750d416342934dd48b3
Rares <rares.andrei.tabacaru@gmail.com>
2023-05-08 13:37:09 +0300

    Added: View model for Adminsitrator's view

---
+++ E-LearningApp/E-LearningApp/ViewModels/AdministratorVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_LearningApp.ViewModels
+{
+    public class AdministratorVM
+    {
+        public int AdminId { get; set; }
+        public AdministratorVM(int id) 
+        {
+            AdminId = id;
+        }
+    }
+}
